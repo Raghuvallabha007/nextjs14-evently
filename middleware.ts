779707f0f667fe3ff59432397c48f3b095b53384
Raghu phaneendra <raghuphaneendra@Raghus-MacBook-Pro.local>
2024-08-09 07:48:54 +0530

    Added logic for Protected/Ignore routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,50 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, getAuth } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export default clerkMiddleware();
 
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+
+  // Define public routes
+  const publicRoutes = [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+
+  // Define routes to ignore (no middleware processing)
+  const ignoredRoutes = [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+
+  // Check if the current route matches any ignore route
+  if (ignoredRoutes.some(route => pathname.startsWith(route))) {
+    return NextResponse.next();
+  }
+
+  // If the current route is public, continue without checking for authentication
+  if (publicRoutes.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  // Check for authentication
+  const { userId } = getAuth(req);
+
+  // If the user is not authenticated, redirect to the sign-in page
+  if (!userId) {
+    const signInUrl = new URL('/sign-in', req.url);
+    return NextResponse.redirect(signInUrl);
+  }
+
+  // If authenticated, proceed to the requested page
+  return NextResponse.next();
+}
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
